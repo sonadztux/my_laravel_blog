89d51ee765e38447d6481264f6e68bc8f75a788e
sonadztux <nanda@anakdesa.id>
2020-01-16 17:51:26 +0700

    creating RoleTableSeeder and UserTableSeeder base

--- database/migrations/2020_01_16_104633_create_role_user_table.php
+++ database/migrations/2020_01_16_104633_create_role_user_table.php
@@ -15,7 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
         });
     }
 
