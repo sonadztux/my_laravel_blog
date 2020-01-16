8021d4289bdcd8492d499d848810f36b2b1f84ac
sonadztux <nanda@anakdesa.id>
2020-01-16 18:06:24 +0700

    edit RoleTableSeeder, UserTableSeeder, and DatabaseSeeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Database\Seeder;
 
 class RoleTableSeeder extends Seeder
@@ -11,6 +12,14 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_regular_user = new Role;
+        $role_regular_user->name = 'user';
+        $role_regular_user->description = 'A regular user';
+        $role_regular_user->save();
+
+        $role_admin_user = new Role;
+        $role_admin_user->name = 'admin';
+        $role_admin_user->description = 'An admin user';
+        $role_admin_user->save();
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Role;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +14,18 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new User;
+        $user->name = 'Nanda Arfan Hakim';
+        $user->email = 'sonadztux@gmail.com';
+        $user->password = bcrypt('k1m0ch1');
+        $user->save();
+        $user->roles()->attach(Role::where('name', 'user')->first());
+
+        $admin = new User;
+        $admin->name = 'Administrator';
+        $admin->email = 'nandarfanhakim@gmail.com';
+        $admin->password = bcrypt("admin123");
+        $admin->save();
+        $admin->roles()->attach(Role::where('name', 'admin')->first());
     }
 }
