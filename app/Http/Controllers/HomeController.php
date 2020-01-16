dcfdc75bc57aac486839520f233e74e5f3e13af7
sonadztux <nanda@anakdesa.id>
2020-01-16 19:29:34 +0700

    Setting up phase 1 fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        if ($request->user()->hasRole('user')) {
+            return redirect('/');
+        }
+
+        if ($request->user()->hasRole('admin')) {
+            return redirect('/admin/dashboard');
+        }
     }
 }
