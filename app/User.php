4bd2bc3cb7b531fcdb0b49849a162c8c8a801c8a
sonadztux <nanda@anakdesa.id>
2020-01-16 18:11:38 +0700

    add checkRoles, hasAnyRole, and hasRole method

--- app/User.php
+++ app/User.php
@@ -40,4 +40,23 @@ class User extends Authenticatable
     public function roles() {
         return $this->belongsToMany(Role::class);
     }
+
+    public function checkRoles($roles) {
+        if (!is_array($roles)) {
+            $roles = [$roles];
+        }
+
+        if (!$this->hasAnyRole($roles)) {
+            auth()->logout();
+            abort(404);
+        }
+    }
+
+    public function hasAnyRole($roles): bool {
+        return (bool) $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasRole($role): bool {
+        return (bool) $this->roles()->where('name', $role)->first();
+    }
 }
