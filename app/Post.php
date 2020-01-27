04eaeb789f6b51ee06e8f190ad73f2c905d931c1
sonadztux <nanda@anakdesa.id>
2020-01-28 04:45:20 +0700

    allowing mass assignment, defining API routes, and create PostResource

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    protected $fillable = ['user_id', 'title', 'body', 'image'];
+    
     public function user() {
         return $this->belongsTo(User::class);
     }
