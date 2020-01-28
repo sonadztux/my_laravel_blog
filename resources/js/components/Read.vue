ff5c27fe9f64df4c57f47489f7a783699df5e7c3
sonadztux <nanda@anakdesa.id>
2020-01-28 19:52:46 +0700

    Building the frontend: Setting up Read view for Admin Dashboard

---
+++ resources/js/components/Read.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="posts">
+        <p class="border p-3" v-for="post in posts">
+            {{ post.title }}
+            <router-link :to="{ name: 'update', params: { postId : post.id } }">
+                <button type="button" class="p-1 mx-3 float-right btn btn-light">
+                    Update
+                </button>
+            </router-link>
+            <button type="button" @click="deletePost(post.id)" class="p-1 mx-3 float-right btn btn-danger">
+                Delete
+            </button>
+        </p>
+        <div>
+            <button v-if="next" type="button" @click="navigate(next)" class="m-3 btn btn-primary">
+                Next
+            </button>
+            <button v-if="prev" type="button" @click="navigate(prev)" class="m-3 btn btn-primary">
+                Previous
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+            this.getPosts();
+        },
+        data() {
+            return {
+                posts: {},
+                next: null,
+                prev: null
+            };
+        },
+        methods: {
+            getPosts(address) {
+                axios.get(address ? address : "/api/posts").then(response => {
+                    this.posts = response.data.data;
+                    this.prev = response.data.links.prev;
+                    this.next = response.data.links.next;
+                });
+            },
+            deletePost(id) {
+                axios.delete("/api/posts/" + id).then(response => this.getPosts())
+            },
+            navigate(address) {
+                this.getPosts(address)
+            }
+        }
+    };
+</script>
