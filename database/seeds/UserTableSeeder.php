149957060501140eae27ee31ff0b233200f949c7
sonadztux <nanda@anakdesa.id>
2020-01-29 00:30:08 +0700

    Changes user and admin account

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,15 +15,15 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $user = new User;
-        $user->name = 'Nanda Arfan Hakim';
-        $user->email = 'sonadztux@gmail.com';
-        $user->password = bcrypt('k1m0ch1');
+        $user->name = 'User';
+        $user->email = 'user@sonadztux.dev';
+        $user->password = bcrypt('dubidam');
         $user->save();
         $user->roles()->attach(Role::where('name', 'user')->first());
 
         $admin = new User;
         $admin->name = 'Administrator';
-        $admin->email = 'nandarfanhakim@gmail.com';
+        $admin->email = 'admin@sonadztux.dev';
         $admin->password = bcrypt("admin123");
         $admin->save();
         $admin->roles()->attach(Role::where('name', 'admin')->first());
