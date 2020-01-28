e3711ab9629352521e851d4ab68d5e568f1a3600
sonadztux <nanda@anakdesa.id>
2020-01-28 20:33:07 +0700

    Adding new routes in VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,34 @@ require('./bootstrap')
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Homepage from './components/Homepage'
+import Create from './components/Create'
 import Read from './components/Read'
+import Update from './components/Update'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     mode: 'history',
-    routes: [{
-        path: '/admin/dashboard',
-        name: 'read',
-        component: Read,
-        props: true
-    }, ],
+    routes: [
+        {
+            path: '/admin/dashboard',
+            name: 'read',
+            component: Read,
+            props: true
+        }, 
+        {
+            path: '/admin/create',
+            name: 'create',
+            component: Create,
+            props: true
+        },
+        {
+            path: '/admin/update',
+            name: 'update',
+            component: Update,
+            props: true
+        }
+    ],
 });
 
 const app = new Vue({
